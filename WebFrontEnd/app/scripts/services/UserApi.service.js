3909e10c0663943a94b2f4c3152baa5f51742826
DESKTOP-3SE1LP0\Mike <whitaker.mj@gmail.com>
2018-06-30 17:09:05 +0200

    Working tests! Happy face!

--- WebFrontEnd/app/scripts/services/UserApi.service.js
+++ WebFrontEnd/app/scripts/services/UserApi.service.js
@@ -1,6 +1,5 @@
 (function() {
   "use strict";
-
   angular.module("webFrontEndApp").factory("UserApi", userApi);
 
   userApi.$inject = ["$resource"];
--- WebFrontEnd/test/spec/controllers/main.js
+++ WebFrontEnd/test/spec/controllers/main.js
@@ -2,43 +2,50 @@
 
 describe("Controller: MainCtrl", function() {
   // load the controller's module
-  var Ctrl, scope, MockUserApi, $q;
-  
-  beforeEach(module('webFrontEndApp'));
+  var Ctrl, $ControllerConstructor, scope, UserApi, User, $q, $rootScope;
 
-  
-  beforeEach(inject(function(_$q_, _$rootScope_, ) {
+  beforeEach(module("webFrontEndApp"));
+
+  // beforeEach(function() {
+  //   module(function($provide) {
+  //     $provide.factory("UserApi", function() {
+  //       debugger;
+  //       return {
+  //         query: function() {
+  //           return {
+  //             $
+  //             deferred.resolve('Hello, ' + name + '!');
+  //           };
+  //         }
+  //       };
+  //     });
+  //   });
+  // });
+
+  beforeEach(inject(function($controller) {
+    $ControllerConstructor = $controller;
+  }));
+
+  beforeEach(inject(function(_$q_, _$rootScope_, _UserApi_, _User_) {
+    UserApi = _UserApi_;
+    User = _User_;
     $q = _$q_;
     $rootScope = _$rootScope_;
   }));
-    
-    
-  beforeEach(inject(function($controller) {
-    scope = $rootScope.$new();
-    
-    MockUserApi = {
-      query: function () {
-        queryDeferred = $q.defer();
-        return { $promise: queryDeferred.promise };
-      }
-    }
 
-    spyOn(MockUserApi, 'query').andCallThrough();
-    
-    $controller('Ctrl', {
-      'UserApi': MockUserApi,
-      'User': {}
-    });
-  }));
+
   
-  describe('MockUserApi.query', function () {
-    beforeEach(function () {
-      queryDeferred.resolve(MockUserApi);
-      $rootScope.$apply();
-    })
-    
-    fit('should query the MockUserApi', function () {
-      expect(MockUserApi.query).toHaveBeenCalled();
+  describe("activate ->", function() {
+    it("should query the UserApi", function() {
+      var spy = spyOn(UserApi, "query").and.callThrough();
+      var Ctrl = $ControllerConstructor("MainCtrl", UserApi, User);
+      expect(spy).toHaveBeenCalled();
+    });
+  });
+
+  describe("activate ->", function() {
+    it("Should test if true is true", function() {
+      expect(true).toBe(true);
     });
   });
 });
